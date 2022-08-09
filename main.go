b2d75a382b4cbbaed6e5f5b2a610891d9938fdb0
azeezdot123 <headofstate123@gmail.com>
2022-08-09 13:37:26 +0100

    create a todo list app

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/thedevsaddam/renderer"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var rnd *renderer.Render
+var db *mgo.Database
+
+const (
+	hostName		string = "localhost:27017"
+	dbName			string = "todo_db"
+	collectionName	string = "todo"
+	port			string = ":9000"
+)
+
+type(
+	todoModel struct{
+		ID			bson.ObjectId `bson:"_id,omitempty"`
+		Title		string `bson:"title"`
+		Completed	bool `bson:"completed"`
+		CreatedAt	time.Time `bson:"createdAt"`
+	}
+	todo struct{
+		ID			string `json:"id"`
+		Title		string `json:"title"`
+		Completed	string `json:"completed"`
+		CreatedAt	time.Time `json:"createdAt"`
+	}
+)
+
+func init() {
+	rnd = renderer.New()
+	sess, err := mgo.Dial(hostName)
+	checkErr(err)
+	sess.SetMode(mgo.Monotonic, true)
+	db = sess.DB(dbName)
+}
+
+func main(){
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Mount("/todo", todoHandlers())
+
+	srv := &http.Server{
+		Addr: port,
+		Handler: r,
+		ReadTimeout: 60*time.Second,
+		WriteTimeout: 60*time.Second,
+		IdleTimeout: 60*time.Second,
+	}
+	go func() {
+		log.Println("Listening on port", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen:%s\n", err)
+		}
+	}()
+}
+
+func todoHandlers() http.Handler {
+	rg := chi.NewRouter()
+	rg.Group(func(r chi.Router){
+		r.Get("/", fetchTodos)
+		r.Post("/", createTodo)
+		r.Put("/{id}", updateTodo)
+		r.Delete("/{id}", deleteTodo)
+	})
+	return rg
+}
+func checkErr(err error){
+	if err != nil {
+		log.Fatal(err)
+	}
+}
